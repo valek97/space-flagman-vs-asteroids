fcee323e1fb9267662caeee1a715bf44a50f394f
valek97 <kritenko97@gmail.com>
2020-10-18 18:20:50 +0300

    Добавил пули, их передвижение, генерацию.

---
+++ Space shutle and asteroids/Assets/Scripts/SpaceshipScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipScript : MonoBehaviour
+{
+    private Rigidbody2D spaceShip;
+    public float speed;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceShip = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spaceShip.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+    }
+}
